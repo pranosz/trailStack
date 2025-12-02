09ffa6ad90849eb8864d8a98700ecdf8d3a72432
Piotr Ranosz <pranosz@wp.pl>
2025-12-02 23:18:14 +0100

    angular material basic setup

--- apps/trial-stack/src/app/app.routes.ts
+++ apps/trial-stack/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Route } from '@angular/router';
+import { Home } from './pages/home/home';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+        path: '',
+        component: Home
+    }
+];
---
+++ apps/trial-stack/src/app/pages/home/home.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatCardModule} from '@angular/material/card';
+import {MatChipsModule} from '@angular/material/chips';
+
+@Component({
+  selector: 'app-home',
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatCardModule, MatChipsModule, MatProgressBarModule],
+  templateUrl: './home.html',
+  styleUrl: './home.scss',
+})
+export class Home {
+  longText = `The Chihuahua is a Mexican breed of toy dog. It is named for the
+  Mexican state of Chihuahua and is among the smallest of all dog breeds. It is
+  usually kept as a companion animal or for showing.`;
+}
